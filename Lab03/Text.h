026787665436a846811b7429d7570802cd9f379e
Elsa Wai Chi Ng <26006006+elsa-ng@users.noreply.github.com>
2017-12-28 14:51:00 -0500

    Declare class Text in Text.h
    class Text is declared in Text.h to include the Rule of 5 and size() member function prototypes.
    
    - Add default constructor "Text()"
    - Add overloaded constructor "Text(const std::string &filename)"
    - Add copy constructor "Text(const Text& t)"
    - Add copy assignment operator "Text& operator=(const Text& t)"
    - Add move constructor "Text(const Text&& t)"
    - Add move assignment operator "Text&& operator=(const Text&& t)"
    - Add "size_t size() const"

--- Lab03/Text.h
+++ Lab03/Text.h
@@ -1,1 +1,28 @@
+/*****************************************
+** Name: Wai Chi Ng						**
+** Student ID: 140634163				**
+** Email: wcng1@myseneca.ca				**
+** Date: September 20, 2017				**
+** OOP345 Section C - Workshop 3		**
+** Text.h								**
+*****************************************/
+
 #pragma once
+
+#include <string>
+
+namespace w3 {
+	class Text {
+		std::string* sArray;
+	public:
+		Text();									// default constructor
+		Text(const std::string &filename);		// overloaded constructor
+		Text(const Text& t);					// copy constructor
+		Text& operator=(const Text& t);			// copy assignment operator
+		Text(const Text&& t);					// move constructor
+		Text&& operator=(const Text&& t);		// move assignment operator
+		~Text();								// destructor
+		size_t size() const;					// member function that returns the number of records of text data
+	}; // class Text
+} // namespace w3
+
